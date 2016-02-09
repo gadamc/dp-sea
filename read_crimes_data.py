9f70e7253d7667e990bae123f4bbe3b8880d2fdc
G Adam Cox <adamcox@us.ibm.com>
2016-02-08 19:07:17 -0800

    Update read_crimes_data.py

--- read_crimes_data.py
+++ read_crimes_data.py
@@ -10,8 +10,9 @@ databaseName = "crimes"
 # in the future will be able to pull not just from database, but from a map-reduce view result!
 # this is available in a Scala notebook now. 
 
-cloudantdata = sqlContext.read.format("com.cloudant.spark").option("cloudant.host",hostname).option("cloudant.username", username).option("cloudant.password",password).load(databaseName).cache()
+cloudantdata = sqlContext.read.format("com.cloudant.spark").option("cloudant.host",hostname).option("cloudant.username", username).option("cloudant.password",password).load(databaseName)
 
+cloudantdata = cloudantdata.cache() #important, or will pull data from cloudant on each 'action'
 cloudantdata.printSchema()
 
 print cloudantdata.count()
